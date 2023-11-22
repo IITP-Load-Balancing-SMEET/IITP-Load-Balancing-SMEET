7e425b8a29dd640c1f723fed5b4f8993b59a83fb
soup1997 <soup1997@naver.com>
2023-11-22 18:15:03 +0900

    2023-11-22

---
+++ HS/vehicle.py
@@ -0,0 +1,14 @@
+import carla
+
+class Vehicle:
+    def __init__(self, role_name='ego'):
+        self.role_name = 'ego'
+
+    def img_callback(self):
+        pass
+
+    def imu_callback(self):
+        pass
+
+    def gnss_callback(self):
+        pass
